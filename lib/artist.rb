9c25c0abee9a6a7fc66bf68c21d991fcd3909361
Keginold Philip <keginoldp@hotmail.com>
2021-04-19 16:49:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,10 @@
-require 'pry'
+attr_accessor :name
+  attr_reader :songs
 
-
-class Artist
-  extend Findable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  extend Findable
   include Paramable
-  attr_accessor :name
-  attr_reader :songs
 
   @@artists = []
 
@@ -17,12 +14,11 @@ class Artist
   end
 
   def self.all
-  @@artists
-end
+    @@artists
+  end
 
-def add_song(song)
+  def add_song(song)
     @songs << song
-    song.artist = self
     songs.each { |song| add_song(song) }
   end
 
