e84b809d80bfa47716241692a75d57507c5e9dc9
Keginold Philip <keginoldp@hotmail.com>
2021-04-19 16:38:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,3 +15,17 @@ class Artist
     super
     @songs = []
   end
+
+  def self.all
+  @@artists
+end
+
+def add_song(song)
+    @songs << song
+    song.artist = self
+
+    songs.each { |song| add_song(song) }
+  end
+
+end
+
